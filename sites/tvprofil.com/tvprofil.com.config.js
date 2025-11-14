fccbeb5477c45749a5c0fb0f9dd85df8dc6e352e
Sehara <iptv.bhiptv@gmail.com>
2025-11-14 15:36:43 +0100

    Update tvprofil.com.config.js

--- sites/tvprofil.com/tvprofil.com.config.js
+++ sites/tvprofil.com/tvprofil.com.config.js
@@ -1,25 +1,34 @@
 const cheerio = require('cheerio')
 const dayjs = require('dayjs')
+
 const WORKER_URL = 'https://red-water-3fc9.seharavip15.workers.dev'
+const USE_PROXY = process.env.NODE_ENV !== 'test' //  ⬅️ test = bez proxy
 
 module.exports = {
   site: 'tvprofil.com',
   days: 2,
+
   url: function ({ channel, date }) {
     const parts = channel.site_id.split('#')
     const query = buildQuery(parts[1], date)
     const targetUrl = `https://tvprofil.com/${parts[0]}/program/?${query}`
-    
+
+    if (!USE_PROXY) return targetUrl //  ⬅️ test traži originalni URL bez proxy-ja
+
     return `${WORKER_URL}?url=${encodeURIComponent(targetUrl)}`
   },
+
   request: {
     headers: {
       'x-requested-with': 'XMLHttpRequest',
-      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36',
-      'referer': 'https://tvprofil.com/tvprogram/',
-      'accept': 'text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01'
+      'user-agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36',
+      referer: 'https://tvprofil.com/tvprogram/',
+      accept:
+        'text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01'
     }
   },
+
   parser: function ({ content }) {
     let programs = []
     const items = parseItems(content)
@@ -40,10 +49,10 @@ module.exports = {
 
     return programs
   },
+
   async channels() {
     const axios = require('axios')
 
-    // prettier-ignore
     const countries = {
       al: { channelsPath: '/al', progsPath: 'al/programacioni', lang: 'sq' },
       at: { channelsPath: '/at', progsPath: 'at/tvprogramm', lang: 'de' },
@@ -53,7 +62,7 @@ module.exports = {
       de: { channelsPath: '/de', progsPath: 'de/tvprogramm', lang: 'de' },
       es: { channelsPath: '/es', progsPath: 'es/programacion-tv', lang: 'es' },
       fr: { channelsPath: '/fr', progsPath: 'fr/programme-tv', lang: 'fr' },
-      hr: { channelsPath: '',    progsPath: 'tvprogram', lang: 'hr' },
+      hr: { channelsPath: '', progsPath: 'tvprogram', lang: 'hr' },
       hu: { channelsPath: '/hu', progsPath: 'hu/tvmusor', lang: 'hu' },
       ie: { channelsPath: '/ie', progsPath: 'ie/tvschedule', lang: 'en' },
       it: { channelsPath: '/it', progsPath: 'it/guida-tv', lang: 'it' },
@@ -66,32 +75,33 @@ module.exports = {
       rs: { channelsPath: '/rs', progsPath: 'rs/tvprogram', lang: 'sr' },
       si: { channelsPath: '/si', progsPath: 'si/tvspored', lang: 'sl' },
       tr: { channelsPath: '/tr', progsPath: 'tr/tv-rehberi', lang: 'tr' },
-      uk: { channelsPath: '/gb', progsPath: 'gb/tvschedule', lang: 'en' },
+      uk: { channelsPath: '/gb', progsPath: 'gb/tvschedule', lang: 'en' }
     }
 
     let channels = []
+
     for (let country in countries) {
       const config = countries[country]
       const lang = config.lang
 
       const targetUrl = `https://tvprofil.com${config.channelsPath}/channels/getChannels/`
-      const url = `${WORKER_URL}?url=${encodeURIComponent(targetUrl)}&callback=cb`
-
-      console.log(url)
+      const url = USE_PROXY
+        ? `${WORKER_URL}?url=${encodeURIComponent(targetUrl)}&callback=cb`
+        : `${targetUrl}?callback=cb`
 
       const cb = await axios
         .get(url, {
           headers: {
             'x-requested-with': 'XMLHttpRequest',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36',
-            'referer': 'https://tvprofil.com/programtv/',
-            'accept': 'text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01',
+            'user-agent':
+              'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36',
+            referer: 'https://tvprofil.com/programtv/',
+            accept:
+              'text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01'
           }
         })
         .then(r => r.data)
-        .catch(err => {
-          console.error(err.message)
-        })
+        .catch(err => console.error(err.message))
 
       if (!cb) continue
 
@@ -117,36 +127,28 @@ module.exports = {
 function parseImage($item) {
   return $item.attr('data-image') || null
 }
-
 function parseDuration($item) {
   return parseInt($item.attr('data-len'))
 }
-
 function parseStart($item) {
   const timestamp = parseInt($item.attr('data-ts'))
   return dayjs.unix(timestamp)
 }
-
 function parseCategory($item) {
   return $item.find('.col:nth-child(2) > small').text() || null
 }
-
 function parseTitle($item) {
   let title = $item.find('.col:nth-child(2) > a').text()
   title += $item.find('.col:nth-child(2)').clone().children().remove().end().text()
-
   return title.replace('®', '').trim().replace(/,$/, '')
 }
-
 function parseItems(content) {
   let data = (content.match(/^[^(]+\(([\s\S]*)\)$/) || [null, null])[1]
   if (!data) return []
   let json = JSON.parse(data)
   if (!json || !json.data || !json.data.program) return []
-
   return [json.data.program]
 }
-
 function buildQuery(site_id, date) {
   const query = {
     datum: date.format('YYYY-MM-DD'),
@@ -157,24 +159,13 @@ function buildQuery(site_id, date) {
   let a = query.datum + query.kanal + c
   let ua = query.kanal + query.datum
 
-  if (
-    typeof ua === 'undefined' ||
-    ua === null ||
-    ua === '' ||
-    ua === 0 ||
-    ua === '0' ||
-    ua !== ua
-  ) {
-    ua = 'none'
-  }
+  if (!ua) ua = 'none'
 
   for (let j = 0; j < ua.length; j++) c += ua.charCodeAt(j)
 
   let i = a.length
   let b = 2
-  while (i--) {
-    b += (a.charCodeAt(i) + c * 2) * i
-  }
+  while (i--) b += (a.charCodeAt(i) + c * 2) * i
 
   b = b.toString()
   const lastCharCode = b.charCodeAt(b.length - 1)
