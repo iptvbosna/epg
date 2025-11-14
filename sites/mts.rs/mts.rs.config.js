6768fcdd7e1c0758ac8fb8cab1e3e053e913e1b1
Sehara <iptv.bhiptv@gmail.com>
2025-11-14 13:56:55 +0100

    Update mts.rs.config.js

--- sites/mts.rs/mts.rs.config.js
+++ sites/mts.rs/mts.rs.config.js
@@ -2,6 +2,17 @@ const axios = require('axios')
 const dayjs = require('dayjs')
 const HttpsProxyAgent = require('https-proxy-agent')
 
+// PROBAJ OVE PROXY SERVERE JEDAN PO JEDAN:
+const proxies = [
+  'http://51.158.68.133:8811',           // Proxy 1
+  'http://proxy.server.com:3128',        // Proxy 2
+  'http://51.158.68.68:8811',            // Proxy 3
+  'http://168.119.53.93:10000',          // Proxy 4
+  'http://103.152.112.162:80'            // Proxy 5
+]
+
+const proxy = proxies[0] // <--- MIJENJAJ BROJ (0,1,2,3,4) I TESTAJ
+
 module.exports = {
   site: 'mts.rs',
   days: 2,
@@ -11,9 +22,12 @@ module.exports = {
     )}&pageSize=10000`
   },
   request: {
-    maxContentLength: 50000000, // 50 Mb
-    httpsAgent: new HttpsProxyAgent('http://proxy.toolip.io:31112'), // BESPLATNI PROXY
-    timeout: 30000
+    maxContentLength: 50000000,
+    httpsAgent: new HttpsProxyAgent(proxy),
+    timeout: 30000,
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+    }
   },
   parser({ content, channel }) {
     const items = parseItems(content, channel)
@@ -32,7 +46,13 @@ module.exports = {
     const data = await axios
       .get(module.exports.url({ date: dayjs() }), module.exports.request)
       .then(r => r.data)
-      .catch(console.error)
+      .catch(err => {
+        console.error('Channel fetch error:', err.message)
+        return null
+      })
+    
+    if (!data || !data.products) return []
+    
     return data.products.map(channel => ({
       lang: 'bs',
       name: channel.name,
